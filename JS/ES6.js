07ca8794de8e6859ec1ebff0859d38f1460adca0
TG2408 <tanmaygupta2408@gmail.com>
2022-01-07 15:20:38 +0530

    adding object extension in ES6

---
+++ JS/ES6.js
@@ -0,0 +1,12 @@
+//  if key and value has same name (identifier), we can just write key omiiting value 
+
+let temp1 = 1;
+    temp2 = 2;
+    
+let obj = {
+    temp1,
+    temp2
+}
+
+console.log(temp1);
+console.log(temp2);
