7f6dab5e01d89c652a68a7776397edff910090e6
TG2408 <tanmaygupta2408@gmail.com>
2022-01-18 13:56:20 +0530

    some changes

--- Python/basics/basics.py
+++ Python/basics/basics.py
@@ -1,10 +1,23 @@
 # pass - break
+# pass is just to avoid error when we dont have any code to execute
+# like below at x == 6 
+
 x = 6
-while x != 0:
+while x > 0:
     print(x)
+
     if x == 6:
+        print("before pass")
         pass
-    else:
+        print("after pass")
+
+    elif x == 3:
+        print("before continue")
+        x -= 1
+        continue
+        print("after continue")
+
+    elif x < 3 :
         print("break")
         break
     x -= 1
