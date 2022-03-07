68a5e493f5b9984ff83936c4cbf10974be2de347
TG2408 <tanmaygupta2408@gmail.com>
2022-03-07 09:55:00 +0530

    bubble sort added

--- Python/5_sorting/1_buuble_sort.py
+++ Python/5_sorting/1_buuble_sort.py
@@ -9,16 +9,40 @@ def bubble_sort(elements):
                 swapped = True
         if not swapped : 
             break
-        
     return elements
 
+def bubble_sort_dict(dictionary, key):
+    size = len(dictionary)
+    
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1):
+            if dictionary[j][key] > dictionary[j+1][key]:
+                dictionary[j], dictionary[j+1] = dictionary[j+1], dictionary[j]
+                swapped = True
+        if not swapped: 
+            break
+    return dictionary
+
 if __name__ == "__main__":
+    # 1st case
     elements = [1,2,4,3,5]
     elements = bubble_sort(elements)
     print(elements)
 
     # python also sort alphabets
-    dictiionary = ["zab", "zza", "aba", "aab"]
-    dictiionary = bubble_sort(dictiionary)
-    print(dictiionary)
+    dictionary = ["zab", "zza", "aba", "aab"]
+    dictionary = bubble_sort(dictionary)
+    print(dictionary)
 
+    # dictionary sort
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
+
+    print( bubble_sort_dict(elements,key='transaction_amount') )
+    print( bubble_sort_dict(elements,key='name') )
+    print( bubble_sort_dict(elements,key='device') )
