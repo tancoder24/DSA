7c505ab30db72c33882509f92c4adf91af2d563a
TG2408 <tanmaygupta2408@gmail.com>
2022-03-11 12:42:14 +0530

    added file

--- Python/6_normal/3_reverse_string.py
+++ Python/6_normal/3_reverse_string.py
@@ -2,12 +2,20 @@ def reverse_func(s):
     s[:] = s[::-1]
     return s
 
+def reverse_func2(s):
+    s = s[::-1]
+    return s
+
 if __name__ == "__main__":
 
     s = list("Tanmay Gupta")
     
     # user defined func
     s = reverse_func(s)
+    print( "".join( s ))
+
+    # user defined func
+    s = reverse_func2(s)
     print( "".join( s ))
     
     # in-built func
