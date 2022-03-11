dd2e2f45e5329606539d19392609b7ebb48b324e
TG2408 <tanmaygupta2408@gmail.com>
2022-03-11 18:01:06 +0530

    NA

--- Python/test1.py
+++ Python/test1.py
@@ -1,8 +1,54 @@
-def temp(s,z = len(s)):
-    print(z)
+class Node:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
 
-a = "As"
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-temp(a, len(a))
+    def insert_at_end(self, val):
+        if self.head == None:
+            self.head = Node(val)
+            return
+        
+        itr = self.head
+        while ( itr.next):
+            itr = itr.next
 
+        itr.next = Node(val)
+    
+    def add_list(self, arr):
+        for a in arr:
+            self.insert_at_end(a)
+
+    def print_list(self):
+        itr = self.head
+        while(itr.next):
+            print(itr.val)
+            itr = itr.next
+
+    def reverseList(self, itr=None):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            head = self.head
+        
+        arr = []
+        arr.append(head.val)
+        
+
+
+        while (head.next):
+            arr.extend(self.reverseList( head.next ))
+        return arr
+
+
+ll = LinkedList()
+ll.add_list([1,2,3,4])
+
+ll.reverseList()
+# ll.print_list()
 
--- Python/test2.py
+++ Python/test2.py
@@ -1,21 +1,64 @@
-total_volunters = input()
-collection_by_volunter = list( map(int, input().split()) )
+def bubble_sort_students(dictionary, key):
+    size = len(dictionary)
+    
+    for i in range(size-1):
+        swapped = False
+        for j in range(size-1):
+            if dictionary[j][key] < dictionary[j+1][key]:
+                dictionary[j], dictionary[j+1] = dictionary[j+1], dictionary[j]
+                swapped = True
+        if not swapped: 
+            break
+    return dictionary
 
-pairs = ["0"]
+if __name__ == "__main__":
 
-for _ in range(int(input())):
-    x, y = input().split()
-    for pair in pairs:
-        if x in pair or y in pair:
-            pair.update([x,y])
-        else: 
-            pairs.append(set([x,y]))
+    # result
+    result = []
 
-for pair in pairs:
-    sum = 0
-    for p in pair:
-        sum += collection_by_volunter[p-1]
-    if sum > max:
-        max = sum
-print(pairs)
-print(max)
+    # students
+    students = []
+
+    # C is no of clg
+    # N is no of student 
+    C, N = list(map(int, input().split()))
+
+    #clg seats index 0 is clg1
+    C_seats = list(map(int, input().split()))
+
+    for i in range(N):
+        stud = input().split(",")
+        student = {
+            "name": stud[0],
+            "percentage": float(stud[1]),
+            "choice1": stud[2],
+            "choice2": stud[3],
+            "choice3": stud[4],
+        }
+        students.append(student)
+
+    # students = bubble_sort_students(students, "percentage")
+    students = sorted(students, key=lambda d: d['percentage'], reverse=True)
+    print(students)
+    
+    for student in students:
+        if C_seats[int(student["choice1"][-1]) - 1] > 0:
+            C_seats[int(student["choice1"][-1]) - 1]  -= 1
+            name = student["name"]
+            clg = student["choice1"]
+            result.append(f"{name} {clg}")
+
+        elif C_seats[int(student["choice2"][-1]) - 1] > 0:
+            C_seats[int(student["choice2"][-1]) - 1]  -= 1
+            name = student["name"]
+            clg = student["choice2"]
+            result.append(f"{name} {clg}")
+
+        elif C_seats[int(student["choice3"][-1]) - 1] > 0:
+            C_seats[int(student["choice3"][-1]) - 1]  -= 1
+            name = student["name"]
+            clg = student["choice3"]
+            result.append(f"{name} {clg}")
+
+    for res in result:
+        print(res)
