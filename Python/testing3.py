422c108724bd81b3162ad7a4efecb439af4c526a
TG2408 <tanmaygupta2408@gmail.com>
2022-04-15 21:02:28 +0530

    work in progress

--- Python/testing3.py
+++ Python/testing3.py
@@ -1,6 +1,37 @@
-for i in  [ 1,2,3]:
-    if i == 3:
-        break 
-    print(i)
-else: 
-    print(30)
+def create_matrix(string):
+    outer_matrix = []
+    n = 0
+    for i in range(4):
+        inner_matrix = []
+        for j in range(4):
+            inner_matrix.append(ord(string[n]))
+            n += 1
+        outer_matrix.append(inner_matrix)
+    return outer_matrix
+
+# padding key
+def pad_key(key_len, key):
+    if key_len < 16:
+        key += " "* (16 - key_len)
+    return key
+
+# padding encrpty string
+def pad_encrp_str(encrp_str_len, encrp_str):
+    if encrp_str_len > 16 and encrp_str_len < 32:
+        encrp_str += " "* (32 - encrp_str_len)
+    elif encrp_str_len < 16:
+        encrp_str += " "* (16 - encrp_str_len)
+    return encrp_str
+
+if __name__ == "__main__":
+
+    encrp_str = input()
+    encrp_str_len = len(encrp_str)
+    encrp_str = pad_encrp_str(encrp_str_len, encrp_str)
+
+    key = input()   
+    key_len = len(key)
+    key = pad_key(key_len, key)
+    key_matrix = create_matrix(key)
+
+    print(key_matrix)
---
+++ Python/testing4.py
@@ -0,0 +1,7 @@
+# temp = ord("C")
+# print(chr(temp), temp)
+
+x = [10, 10, 10, 10]
+
+print( x^4 )
+print( " "*5 )
