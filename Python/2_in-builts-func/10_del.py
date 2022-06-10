3e677612f32cd12fb2b93d0454a371a9a99f1194
tancoder24 <tanmaygupta2408@gmail.com>
2022-06-10 12:26:27 +0530

    upated inbuilt datatypes

--- Python/2_in-builts-func/10_del.py
+++ Python/2_in-builts-func/10_del.py
@@ -1,3 +1,7 @@
 x = [1,2,3]
 
+del x[1]
+print(x)
+
 del x
+print(x)
---
+++ Python/3_datatypes/0_built-in-datatypes/4_dictionary.py
@@ -0,0 +1,33 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# to copy 
+mydict = thisdict.copy()
+mydict = dict(thisdict)
+
+# to acces dict items
+x = thisdict["model"]
+x = thisdict.get("model")
+
+# Nested Dictionaries
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
