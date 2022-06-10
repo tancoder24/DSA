4e1453bedd5dece9c1667e55e0711e72c52c2cc6
tancoder24 <tanmaygupta2408@gmail.com>
2022-06-10 23:29:48 +0530

    iter updated

--- Python/1_basics/6_iter-next.py
+++ Python/1_basics/6_iter-next.py
@@ -1,8 +1,34 @@
 arr = ["apple", "banana", "orange"]
 
 x = iter(arr)
-
 print(x)
 print(next(x))
 print(next(x))
 print(next(x))
+
+###########################################
+
+class myClass:
+    def __iter__(self):
+        self.a = 1        
+        return self
+
+    def __next__(self):
+        if self.a > 3:
+            raise StopIteration
+        ans = self.a
+        self.a += 1
+        return ans
+
+a = myClass()
+b = iter(a)
+
+for x in b:
+  print(x)
+
+
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
