d8a7f2f6f94942b74e5af825a79e4bde63d5cdb4
tancoder24 <tanmaygupta2408@gmail.com>
2022-06-11 18:54:05 +0530

    added imp library

---
+++ Python/7_imp_library/1_math/1_math.py
@@ -0,0 +1,15 @@
+# inbuilt math functions
+
+print( pow(2,3) )
+print( abs(-7) )
+print( min(3,4,5) )
+print( max(3,4,5) )
+
+# math module
+import math
+print("MATH MODULE")
+print( math.sqrt(25) )
+print( math.ceil(5.5) )
+print( math.floor(5.5) )
+print( math.pi )
+print( dir(math) )
---
+++ Python/7_imp_library/4_datetime/1_datetime.py
@@ -0,0 +1,7 @@
+import datetime
+
+birth_date = datetime.datetime(2001, 8, 24)
+print( f"birth date { birth_date }" )
+
+current_date = datetime.datetime.now()
+print( f"current date { current_date }" )
