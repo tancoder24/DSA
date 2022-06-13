83d091b6ca63ec76ee386c52b278a3040da4e11a
tancoder24 <tanmaygupta2408@gmail.com>
2022-06-13 23:01:25 +0530

    .

--- Python/8_temp/testing4.py
+++ Python/8_temp/testing4.py
@@ -1,10 +1,21 @@
-# from audioop import reverse
+def sort012(arr,n):
+    # code here
+    zero = 0
+    one = 0
+    two = n-1
+        
+    while one < two:
+        if arr[one] == 2:
+            arr[one], arr[two] = arr[two], arr[one]
+            two -= 1
+        elif arr[one] == 0:
+            arr[one], arr[zero] = arr[zero], arr[one]
+            one += 1
+            zero += 1
+        elif arr[one] == 1:
+            one += 1
+    return arr
 
+arr = [0,1,0]
 
-# print( hash("Tanmay") )
-# print( help() )
-
-x = "tanmay"
-x = reversed(x)
-x = list(x)
-print(x)
+ans = sort012(arr,3)
