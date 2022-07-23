c3fb229bbb4bb8df1e360ba34241ab6a7b819903
tancoder24 <tanmaygupta2408@gmail.com>
2022-07-23 12:21:05 +0530

    print keypad added

--- Python/5_algorithms/3_recursion/19_print_keypad.py
+++ Python/5_algorithms/3_recursion/19_print_keypad.py
@@ -7,11 +7,10 @@ def print_keypad(n, keypad, output=""):
 
     # 2. recurrsive call
     for c in keypad[ int(n[0])-1 ]:
-        output += c
-        print_keypad(n[1:] , keypad, output)
+        print_keypad(n[1:] , keypad, output+c)
 
 if __name__ == "__main__":
-    n = "1"
+    n = "23"
     keypad = ["", "ABC", "DEF", "GHI", "JKL", "MNO", "PQR", "STU", "VWX", "YZ"]
 
     print_keypad(n, keypad)
